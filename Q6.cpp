b3498b312c0acbbad20fce2452705bce7a4bc72a
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-12-11 00:12:31 +0530

    Update Q6.cpp

--- Q6.cpp
+++ Q6.cpp
@@ -1,1 +1,42 @@
-// 6th file
+// 6th file SELECTION SORT
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        // Find the index of the minimum element in the unsorted part
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+    
+    cout << "Original array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr);
+
+    cout << "Sorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
